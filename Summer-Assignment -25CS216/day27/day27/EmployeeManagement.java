cb20be6233f95b75f32e1a41fb8f120f8801720f
ishujaiswal9007-pixel <ishujaiswal9007@gmail.com>
2026-06-27 23:25:16 +0530

    Add files via upload

---
+++ Summer-Assignment -25CS216/day27/day27/EmployeeManagement.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class Employee {
+    int empId;
+    String empName;
+    double salary;
+
+    Employee(int empId, String empName, double salary) {
+        this.empId = empId;
+        this.empName = empName;
+        this.salary = salary;
+    }
+
+    void display() {
+        System.out.println("Employee ID: " + empId);
+        System.out.println("Employee Name: " + empName);
+        System.out.println("Salary: " + salary);
+    }
+}
+
+public class EmployeeManagement {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Employee ID: ");
+        int id = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Employee Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter Salary: ");
+        double salary = sc.nextDouble();
+
+        Employee e = new Employee(id, name, salary);
+
+        System.out.println("\nEmployee Details");
+        e.display();
+    }
+}
